59b18e881453805d49e5a45b4968314c80745fc8
MOSTAFA SHARIATZADEH <shrytzadh@gmail.com>
2021-08-07 13:16:14 +0430

    Add Notice to file

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 
+// Notice!
 // Please assign the following variables According to what is said in the documentation.
-
+// If you want to enter only English data then it is better to delete the line 14.
+ 
 try {
     $host    = "<HOST_NAME>";
     $DB_name = "<DATABASE_NAME>";
